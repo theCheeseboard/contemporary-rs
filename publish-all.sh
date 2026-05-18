3cd040f0219fc5ddadcc03dea65548b7668db383
Victor Tran <vicr12345@gmail.com>
2026-05-18 19:17:30 +1000

    Update publish script

--- publish-all.sh
+++ publish-all.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+crates=(
+  "localesupport/cntp_localesupport"
+  "i18n/cntp_i18n_core"
+  "i18n/cntp_i18n_build_core"
+  "i18n/cntp_i18n_parse"
+  "i18n/cntp_i18n_gen"
+  "i18n/cntp_i18n_macros"
+  "i18n/cntp_i18n"
+  "i18n/cargo_cntp_i18n"
+  "i18n/cntp_i18n_parlance_source"
+
+  "cntp_config"
+  "icon_tool/cntp_icon_tool_core"
+  "icon_tool/cntp_icon_tool_macros"
+  "deploy_tool/cntp_bundle_lib"
+  "deploy_tool/cargo_cntp_deploy"
+  "deploy_tool/cargo_cntp_bundle"
+)
+
 function publish_crate() {
-    pushd $1
-    echo "##### $1 #####"
-    cargo publish
-    echo "##############"
-    echo
-    echo
-    popd
+    echo "Publishing crate $1"
+    pushd "$1" > /dev/null || return
+
+    if ! cargo_output=$(cargo publish --no-verify 2>&1); then
+      echo "$cargo_output"
+      echo "Crate $1 could not be published"
+      exit 1
+    fi
+
+    popd > /dev/null || exit
 }
 
-publish_crate localesupport/cntp_localesupport
-publish_crate i18n/cntp_i18n_core
-publish_crate i18n/cntp_i18n_build_core
-publish_crate i18n/cntp_i18n_parse
-publish_crate i18n/cntp_i18n_gen
-publish_crate i18n/cntp_i18n_macros
-publish_crate i18n/cntp_i18n
-publish_crate i18n/cargo_cntp_i18n
-publish_crate i18n/cntp_i18n_parlance_source
-
-publish_crate cntp_config
-publish_crate icon_tool/cntp_icon_tool_core
-publish_crate icon_tool/cntp_icon_tool_macros
-publish_crate deploy_tool/cntp_bundle_lib
-publish_crate deploy_tool/cargo_cntp_deploy
-publish_crate deploy_tool/cargo_cntp_bundle
+for crate in "${crates[@]}"; do
+  publish_crate "$crate"
+done
